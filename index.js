4f8ba28a11c713c271faf36c71b7a7bb52c2d0d6
Souravkr-00 <cash.souravkr12@gmail.com>
2024-07-13 18:48:01 +0530

    Real location tracker app

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const express = require('express');
+const path = require("path");
+const {Server} = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname,"public")));
+
+io.on("connection",(socket)=>{
+    socket.on("send-location",function(data){
+        io.emit("receive-location",{id:socket.id,...data});
+    })
+    console.log("Hello from User id:",socket.id);
+})
+
+app.use(express.static(path.resolve("./public")));
+
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+server.listen(9000,() => console.log("Server running in 9000"));
